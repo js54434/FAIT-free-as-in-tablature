b94e10cd1c593479a036c520c700d61ff75e846c
js54434 <js54434@gmail.com>
2014-02-05 12:38:32 -0500

    fixed bug where repeating notes get clipped

--- MidiPlayer.py
+++ MidiPlayer.py
@@ -29,8 +29,8 @@ class MidiPlayer:
     def setTempo(self, tempo):
         self.tempo = tempo
                 
-    def playNote(self, parent, trackNum, pitch, volume=96, duration=-1):
-        worker = PlayNote(self, self.fs, trackNum, pitch, volume, duration)
+    def playNote(self, parent, trackNum, pitch, volume=96, duration=-1, sameNote='False'):
+        worker = PlayNote(self, self.fs, trackNum, pitch, volume, duration, sameNote)
         worker.start()
         
         return worker
@@ -77,15 +77,32 @@ class Playback:
                 elif val > -1:
                     # stop any notes that are still playing on the string
                     noteInfo = [x for x in self.notesPlaying if x[1] == i and x[2] == j]
+                    newPitch = self.tracks[i].convertNumberToPitch(j, val)
+
                     if len(noteInfo) > 0:
-                        noteInfo = noteInfo[0]
-                        noteInfo[0].stopNote()
-                        self.notesPlaying.remove(noteInfo)
-                    
-                    pitch = self.tracks[i].convertNumberToPitch(j, val)
-                    noteThread = self.midiPlayer.playNote(self, i, pitch)
-                    # store trackNum, stringNum, and thread object of currently playing notes
-                    self.notesPlaying.append([noteThread, i, j])
+                        # if new pitch is different, stop note within 50 ms
+                        if noteInfo[0][0].pitch != newPitch:
+                            noteInfo = noteInfo[0]
+                            noteInfo[0].stopNote()
+                            self.notesPlaying.remove(noteInfo)
+                            
+                            noteThread = self.midiPlayer.playNote(self, i, newPitch)
+                            # store trackNum, stringNum, and thread object of currently playing notes
+                            self.notesPlaying.append([noteThread, i, j])
+
+                        # if pitch is the same, stop note immediately so order of 
+                        # noteon and noteoff don't get mixed up
+                        else:
+                            noteInfo = noteInfo[0]
+                            noteInfo[0].stopNote()
+                            self.notesPlaying.remove(noteInfo)
+
+                            noteThread = self.midiPlayer.playNote(self, i, newPitch, sameNote='True')
+                            self.notesPlaying.append([noteThread, i, j])
+                            
+                    else:
+                        noteThread = self.midiPlayer.playNote(self, i, newPitch)
+                        self.notesPlaying.append([noteThread, i, j])
                     
         # update graphics by moving cursor
         self.cursorItem.moveRight()
@@ -100,7 +117,7 @@ class Playback:
 
 
 class PlayNote(threading.Thread):
-    def __init__(self, parent, fs, trackNum, pitch, volume, duration):
+    def __init__(self, parent, fs, trackNum, pitch, volume, duration, sameNote):
         threading.Thread.__init__(self)
         self._parent = parent
         self.trackNum = trackNum
@@ -109,31 +126,39 @@ class PlayNote(threading.Thread):
         self.duration = duration
         self.fs = fs        # fluidsynth object
         
+        self.sameNote = sameNote        # determines whether new note has same pitch as before
         self.event = threading.Event()
         
     def run(self):
         self.startTime = time.time()
+        if self.sameNote == 'True':
+            self.fs.noteoff(0, self.pitch)
         self.fs.noteon(0, self.pitch, self.volume)
+
+        checkTime = 0.050
 
         # if self.duration was set to -1, play indefinitely
         if self.duration == -1:
             while not self.event.is_set():
-                self.event.wait(0.1)        # wait 100 ms
-            self.fs.noteoff(0, self.pitch)
+                self.event.wait(checkTime)        # wait 100 ms
+            if self.sameNote == 'False':
+                self.fs.noteoff(0, self.pitch)
         
         elif self.duration > 0:
             # wait and check roughly every 100 ms to see if something stops it by calling self.event.set()
-            num = int(math.ceil(self.duration / 0.1));
+            num = int(math.ceil(self.duration / checkTime));
             dt = self.duration / num
             for i in range(num):
                 if self.event.is_set() or (self.duration < time.time() - self.startTime):
                     break
                 self.event.wait(dt)
 
-            self.fs.noteoff(0, self.pitch)
+            if self.sameNote == 'False':
+                self.fs.noteoff(0, self.pitch)
         
         else:
             print('Warning: note duration of ' + str(self.duration) + ' is not a valid number')
     
     def stopNote(self):
         self.event.set()
+        
